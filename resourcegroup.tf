0ac37b1e5b677db2ace14c249773af4c0693fb82
Sami Islam <islam.samipkr@gmail.com>
2025-09-10 19:30:50 -0400

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -6,3 +6,7 @@ resource "azurerm_resource_group" "terraformseptember" {
   name     = "mcit_resource_group_sep092025"
   location = "canadacentral"
 }
+resource "azurerm_resource_group" "tfsept2025" {
+  name     = "mcit_si_sept_2025"
+  location = "canadacentral"
+}
