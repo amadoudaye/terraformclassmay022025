9125f87941867221b0359c1746e88e1b2166dc35
amadoudaye <amadoudaye@gmail.com>
2025-10-24 20:06:22 -0400

    Refactor variable.tf to clean up and add new variables
    Removed several unused variables and added new variables for environments and service names.

--- variable.tf
+++ variable.tf
@@ -1,63 +1,26 @@
-variable "subscription_id"{
-  type=string
+variable "environments_second" {
+  default = ["dev", "qa", "stage", "prod"]
 }
-variable "client_id"{
-  type=string
+ 
+variable "service_names_town" {
+  default = ["montreal", "toronto", "calgary", "ottawa"]
 }
-variable "client_secret"{
-  type=string
+ 
+locals {
+  # Validate lengths match (optional but good practice)
+  env_service_map = zipmap(var.environments_second, var.service_names_town)
 }
-variable "tenant_id"{
-  type=string
+ 
+resource "azurerm_storage_account" "mcitstoremay2025second" {
+  for_each = local.env_service_map
+ 
+  name                     = each.value
+  resource_group_name      = "mcitresources"
+  location                 = "East US"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
-variable "storage_account_name"{
-  type=string
-  default="mcitstoragemay"
-}
-variable "account_tier"{
-  type=string
-  default="Premium"
-}
-variable "account_replication_type"{
-  type=string
-  default="ZRS"
-}
-variable "container_access_type"{
-type=string
-default="private"
-}
-
-variable "environment"{
-  type=string
-  default="production"
-}
-
-variable "storagecontainermay"{
-  type=string
-  default="mcitsstoragecontmay"
-}
-
-variable "storageblobmay"{
-  type=string
-  default="mcitsstorageblobmay"
-}
-variable "blob_type"{
-  type=string
-  default="Block"
-}
-variable "blob_source"{
-type=string
-default="some-local-file.zip"
-}
-variable "full_text" {
-type=string
-  default = "I am a student at mcit"
-}
-variable "classwork" {
-type=string
-  default = "I love the country called Canada, it is awesome"
-}
-variable "name"{
-type=string
-default="onome"
+ 
+output "mcit16may2025second" {
+  value = local.env_service_map
 }
